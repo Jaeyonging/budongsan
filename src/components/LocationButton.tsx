fd2061edab6d6afbd3d353a1519c89d2870f95dc
jaeyonging <wodyd1318@naver.com>
2024-07-22 18:25:30 +0900

    add categories

--- src/components/LocationButton.tsx
+++ src/components/LocationButton.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 export const LocationButton = ({ title, setCurrCityNumber, cityNumber, currentNumber }: Props) => {
-    console.log(cityNumber, " dfasdfsadfsad", currentNumber)
     return (
         <button
             className={`bg-[red] p-2 rounded-[10px] m-[10px] hover:bg-[green] ${cityNumber == currentNumber && "bg-[#18fa8d]"}`}
