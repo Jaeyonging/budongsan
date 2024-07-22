1154477379678a5886eba612da5c44ef3204f5d7
jaeyonging <wodyd1318@naver.com>
2024-07-22 16:27:51 +0900

    fixing

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,6 +14,7 @@ export async function GetHouseMonthData(code: string, date: string) {
         console.log(response)
         return response.data.response.body.items.item
     }).catch((err) => {
+        console.log(err)
         return err
     });
 }
